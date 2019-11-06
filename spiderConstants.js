eb26a509630e3f3b2d2e559e657390b4a5601b5a
breezymelon <935448078@qq.com>
2019-11-06 20:15:04 +0800

    [Delete] delete test.js

--- spiderConstants.js
+++ spiderConstants.js
@@ -1,6 +1,5 @@
 module.exports = {
   OPERA_PERIODS: ['上古', '商', '周', '秦', '楚汉', '西汉', '新', '东汉', '三国', '晋', '南北朝', '隋', '唐', '五代十国', '宋', '元', '明', '清', '现代'],
-  // OPERA_PERIODS: ['明'],
   OPERA_PERIOD_LIST_URL: 'http://repertoire.xikao.com/list?period=',
   OPERA_SEARCH_URL: 'http://scripts.xikao.com/search?q=',
   OPERA_DETAIL_URL: 'http://scripts.xikao.com/play/'
--- test.js
+++
@@ -1,9 +0,0 @@
-require('./env')
-const Opera = require('./models/Opera')
-
-const getAllOperaLyrics = async () => {
-  const operas = await Opera.getAllOperas()
-  const ids = operas.map(opera => opera.dataValues.operaId)
-  console.log(ids)
-}
-getAllOperaLyrics()
